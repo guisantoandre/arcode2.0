bf10decd453211d7f293cd86befeb592eeeaba6e
guisantoandre <gui.santoandre@gmail.com>
2023-05-12 16:44:01 -0300

    ajuste layout contato

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -19,6 +19,7 @@ export default function Form() {
 
       if (name === "" || email === "" || message === "") {
          toastWarningForm();
+         setIsSending(false);
          return;
       }
 
